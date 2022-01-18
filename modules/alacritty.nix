7feb6d08dd4aa707985e1b092b3c51168d24b808
Jack Hendricks <64325143+JDOG787@users.noreply.github.com>
2022-01-17 17:53:34 -0700

    add font for alacritty

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -1,14 +1,22 @@
 {config, pkgs, ...}: 
 
-{
+let 
+  terminalFont = { family = "JetBrains Mono"; };
+
+in {
   programs.alacritty = {
     enable = true;
     
     settings = {
-      background_opacity = 0.4;
+      background_opacity = 0.5;
+      background_color = "#2e3440";
       
       fonts = {
-        normal = "JetBrainsMono";
+        normal = terminalFont;
+	bold = terminalFont;
+	italic = terminalFont;
+	italic_bold = terminalFont;
+	size = "12";
       };
      
     };
